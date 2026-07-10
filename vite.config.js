a5bf6292064316a695c62bc97e6cd69938af5a59
Pavithran1823 <paviofficiall023@gmail.com>
2026-07-10 18:20:28 +0530

    fix: add modernPolyfills and global error handler for iPhone debugging

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [
     react(),
     legacy({
-      targets: ['defaults', 'not IE 11']
+      targets: ['defaults', 'not IE 11'],
+      modernPolyfills: true
     })
   ],
   build: {
