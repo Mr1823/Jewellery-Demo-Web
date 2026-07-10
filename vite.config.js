f8869a6e0a521db86093c9f1123d1c97108bc803
Pavithran1823 <paviofficiall023@gmail.com>
2026-07-10 19:07:59 +0530

    fix: remove vite legacy plugin completely to prevent core-js polyfill Safari crash

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+    react()
   ],
   build: {
-    target: 'es2015',
+    target: 'esnext',
   }
 })
